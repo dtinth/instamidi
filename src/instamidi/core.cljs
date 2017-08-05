b6bac903ae81684757431b158a5673b08ea0a173
Thai Pangsakulyanont <dtinth@spacet.me>
2017-08-05 16:19:57 +0700

    ✨🎉 CAN PARSE MIDI FILE NOW ^_^

--- src/instamidi/core.cljs
+++ src/instamidi/core.cljs
@@ -13,8 +13,34 @@
   [:div
     [:pre {:wrap true} (str @app-state)]])
 
-(defn convert-midi [midi-file]
-  (vec (.getMidiEvents midi-file)))
+(defn event->notepresses
+  [events]
+  (let [get-time #(.-playTime %)
+        note-ons (filter #(= (.-subtype %) 9) (sort-by get-time events))
+        note-offs (filter #(= (.-subtype %) 8) (sort-by get-time events))
+        join-events (fn [on off]
+                      {:velocity (.-param2 on)
+                       :start (.-playTime on)
+                       :end (.-playTime off)})]
+    (map join-events note-ons note-offs)))
+
+(defn map-vals
+  [f map]
+  (into {} (for [[k v] map] [k (f v)])))
+
+(defn convert-midi
+  [midi-file]
+  (let [is-note-event #(or (= (.-subtype %) 8)
+                           (= (.-subtype %) 9))
+        note-value #(.-param1 %)]
+    (->>
+      (.getMidiEvents midi-file)
+      (filter is-note-event)
+      (group-by note-value)
+      (map-vals event->notepresses)
+      (vals)
+      (flatten)
+      (sort-by :start))))
 
 (defn handle-loaded! [notes]
   (swap! app-state assoc :loaded true :notes notes))
